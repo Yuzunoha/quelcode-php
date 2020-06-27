7b361ad398455d8d362986fe94a896a841fdfc36
Yuzunoha <studysteadystandby@gmail.com>
2020-06-27 21:09:29 +0900

    いいね実行の判定を途中まで書いた

--- html/php-challenge/Lib.php
+++ html/php-challenge/Lib.php
@@ -32,19 +32,50 @@ class Lib
 	{
 	}
 
+	/**
+	 * オリジナルポストidを取得する
+	 * 引数がオリジナルポストidだったなら同じ値を返却する
+	 */
+	public static function getOriginalPostId(int $postId): int
+	{
+		foreach (self::$retweets as $row) {
+			if (intval($row['retweet_post_id']) === $postId) {
+				return intval($row['original_post_id']);
+			}
+		}
+		return $postId;
+	}
+
 	public static function handleLike()
 	{
-		// いいねボタンが押された投稿のid
-		$likePostId = $_REQUEST['like'];
-		if (!isset($likePostId)) {
+		if (!isset($_REQUEST['like'])) {
 			/* いいねボタンは押されていない */
 			return;
 		}
-		/* いいねボタンが押された */
-		// 自分のid
-		$myId = $_SESSION['id'];
 
-		echo 'myId : ' . $myId . '<br>';
-		echo 'likedPostId : ' . $likePostId . '<br>';
+		// いいねボタンが押された投稿のidを取得する
+		$likePostId = intval($_REQUEST['like']);
+
+		// 自分のidを取得する
+		$myId = intval($_SESSION['id']);
+
+		// オリジナルポストidを取得する
+		$originalPostId = self::getOriginalPostId($likePostId);
+
+		// 既にいいねしているかどうか判定する
+		$amILiked = false;
+		foreach (self::$likes as $row) {
+			if (intval($row['member_id']) === $myId) {
+				if (intval($row['post_id']) === $originalPostId) {
+					$amILiked === true;
+				}
+			}
+		}
+
+		if ($amILiked) {
+			/* 既にいいねしているので、いいねを取り消す */
+		} else {
+			/* まだいいねしていないので、いいねを追加する */
+		}
 	}
 }
